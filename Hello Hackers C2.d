9aa9bfa8ba6fb65ef4757f6a230e559d0718df8d
medhan-sh <medshon@proton.me>
2025-09-23 20:17:27 +0530

    Create Hello Hackers C2.d

---
+++ Hello Hackers C2.d
@@ -0,0 +1,22 @@
+# Challenge Name
+
+### Hello Hackers: Intro To Arguments 
+
+**Flag:** `pwn.college{sJchFAHHPBq1AOCEvEqTCfcW5zK.QX4YjM1wiN2kjNzEzW}`
+
+I successfully completed the challenge by adhering to the provided instructions
+
+```
+hacker@hello~intro-to-arguments:~$ hello hackers
+Success! Here is your flag:
+pwn.college{sJchFAHHPBq1AOCEvEqTCfcW5zK.QX4YjM1wiN2kjNzEzW}
+```
+
+## What I learned
+
+>Understood the concept of arguments.
+>Argument is extra data that is added to the command  
+
+## References
+
+None
